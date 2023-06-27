6f9d9f78d5dc9dc51ddb13d9fcd3f0e809c8b150
F1nal04 <leongaborbojanowski04@gmail.com>
2023-06-27 09:37:00 +0200

    Datumseingabe und submitButton

--- Welcome.java
+++ Welcome.java
@@ -1,12 +1,17 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class Welcome extends JFrame {
     private JLabel welcomeLabel;
-    private JLabel dateLabel;
+    private JLabel currentDateLabel;
+    private JLabel enteredDateLabel;
     private JLabel nameLabel;
+    private JTextField dateTextField;
+    private JButton submitButton;
 
     private String getCurrentDate() {
         LocalDate currentDate = LocalDate.now();
@@ -19,11 +24,25 @@ public class Welcome extends JFrame {
         setLayout(new FlowLayout());
 
         welcomeLabel = new JLabel("Willkommen!");
-        dateLabel = new JLabel("Heutiges Datum:" + getCurrentDate());
+        currentDateLabel = new JLabel("Heutiges Datum:" + getCurrentDate());
+        enteredDateLabel = new JLabel("Eingegebenes Datum:");
         nameLabel = new JLabel("Name:" + name);
+        dateTextField = new JTextField(10);
+        submitButton = new JButton("Datum bestätigen");
+
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String enteredDate = dateTextField.getText();
+                enteredDateLabel.setText("Eingegebenes Datum:" + enteredDate);
+            }
+        });
 
         add(welcomeLabel);
         add(nameLabel);
-        add(dateLabel);
+        add(currentDateLabel);
+        add(enteredDateLabel);
+        add(dateTextField);
+        add(submitButton);
     }
 }
